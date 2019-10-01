f6afcc6b2ecbe2bc2db9864033a882954cff9308
Cao Mai <caomai@rocketmail.com>
2019-09-30 20:15:56 -0700

    Users can destroy playlists

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def playlists_new():
     """Create a new playlist."""
     return render_template('playlists_new.html', playlist={}, title='New Playlist')
 
-@app.route("/playlists/delete")
-def playlists_delete():
+@app.route("/playlists/delete_all")
+def playlists_delete_all():
     '''deletes collection in mongbd'''
     playlists.remove({})
     return render_template("playlists_index.html")
@@ -48,6 +48,18 @@ def playlists_submit():
     playlist_id = playlists.insert_one(playlist).inserted_id
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
     """Submit an edited playlist."""
@@ -61,11 +73,11 @@ def playlists_update(playlist_id):
         {'$set': updated_playlist})
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
-@app.route('/playlists/<playlist_id>/edit')
-def playlists_edit(playlist_id):
-    """Show the edit form for a playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
-    
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """Delete one playlist."""
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
+
 if __name__ == "__main__":
     app.run(debug=True)
